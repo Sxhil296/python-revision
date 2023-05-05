3f23548803e2fb6e0d51ce5c5ec5835c22b31a90
Sahil <leviack629@gmail.com>
2023-05-05 22:48:17 +0530

    identation

---
+++ indentation.py
@@ -0,0 +1,18 @@
+"""
+Indentation refers to the spaces at the beginning of a code line.
+
+Python uses indentation to indicate a block of code.
+
+Python will give you an error if you skip the indentation.
+
+The number of spaces is up to you as a programmer,
+the most common use is four, but it has to be at least one.
+
+1 tab = 4 spaces
+
+"""
+
+if 5 > 2:
+    print("Five is greater than two.")
+else:
+    print("Two is greater than five.")
