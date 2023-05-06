73303fa7eab3b3d7824055f22e6b13629409c117
Sahil <leviack629@gmail.com>
2023-05-06 23:47:10 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+#Variables are containers for storing data values.
+
+x = 5
+print(x)
+
+#Variables do not need to be declared with any particular type.
+y = 6
+y = "Salt"
+print(y)
+
+#If you want to specify the data type of a variable, this can be done with casting.
+z = str(3)
+a = int(7)
+print(z)
+print(a)
+
+#You can get the data type of a variable with the type() function.
+print(type(x))
+
+#String variables can be declared either by using single or double quotes.
+name = "Python"
+#is same as
+name = 'Python'
+
+#Variable names are case-sensitive.
+ c = 10
+ C = "Sally"
